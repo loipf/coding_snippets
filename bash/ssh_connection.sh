9a4f4eeee3ea44e85dbed0a1b359f78df49801bb
loipf <49686556+loipf@users.noreply.github.com>
2020-10-01 14:43:40 +0200

    added mounting script

--- bash/ssh_connection.sh
+++ bash/ssh_connection.sh
@@ -1,3 +1,14 @@
 
 ### ssh forwarding to local pc
 ssh -o 'ProxyCommand ssh -W %h:%p user@server' user@computer
+
+
+### ssh mounting on local machine
+user_id="p5"
+
+mkdir ~/mount_dir/
+fusermount -u -z ~/mount_dir/
+sshfs -ocache=yes,kernel_cache,compression=no,default_permissions,idmap=user,uid=$(id -u),gid=$(id -g),follow_symlinks $user_id@SERVER_ADRESS:/home/ ~/mount_dir/
+
+
+
