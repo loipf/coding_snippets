9c01e599fd9b87d98dc40e0411f62fb56c918178
loipf <49686556+loipf@users.noreply.github.com>
2020-10-22 16:24:28 +0200

    Update small_functions.R

--- R/small_functions.R
+++ R/small_functions.R
@@ -223,5 +223,20 @@ find_common_substring_beginning <- function(words) {
   #return substring
   substring(words[1], 1, neqal)
 }
-
-
+                            
+                            
+                            
+                            
+                            
+                            
+### from: https://stackoverflow.com/questions/18964837/fast-correlation-in-r-using-c-and-parallelization
+BLAS_cor_on_rows <- function(df) {
+  mat = as.matrix(df)
+  mat = mat - rowMeans(mat)  # center each variable
+  mat = mat / sqrt(rowSums(mat^2)) # standardize each variable
+  cr = tcrossprod(mat)  # calculate correlation
+  return(cr)
+}
+                            
+                            
+                            
