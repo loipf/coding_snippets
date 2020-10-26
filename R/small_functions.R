a376b6a6855cb7b93c9ca26970bdfdab7ba72ae4
loipf <49686556+loipf@users.noreply.github.com>
2020-10-26 09:17:06 +0100

    moved BLAS functions into separate file

--- R/small_functions.R
+++ R/small_functions.R
@@ -226,17 +226,5 @@ find_common_substring_beginning <- function(words) {
                             
                             
                             
-                            
-                            
-                            
-### from: https://stackoverflow.com/questions/18964837/fast-correlation-in-r-using-c-and-parallelization
-BLAS_cor_on_rows <- function(df) {
-  mat = as.matrix(df)
-  mat = mat - rowMeans(mat)  # center each variable
-  mat = mat / sqrt(rowSums(mat^2)) # standardize each variable
-  cr = tcrossprod(mat)  # calculate correlation
-  return(cr)
-}
-                            
-                            
+   
                             
