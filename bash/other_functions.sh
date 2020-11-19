65d3c282c1861d4ca2b088fbc634a8e7e427fe6b
loipf <49686556+loipf@users.noreply.github.com>
2020-11-19 11:50:03 +0100

    Update other_functions.sh

--- bash/other_functions.sh
+++ bash/other_functions.sh
@@ -8,3 +8,13 @@ echo_time() {
 
 echo_time "download .fa files"
 
+
+
+
+
+### outputs number of rows and columns of a file (specify FS separator)
+awk 'BEGIN{FS="\t"}END{print "COLUMN NO: "NF " ROWS NO: "NR}' file
+
+
+
+
