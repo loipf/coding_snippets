030a8f2b940d4c0120434e5d72f178b81941ea5a
loipf <49686556+loipf@users.noreply.github.com>
2020-11-25 11:50:12 +0100

    Update folder_handling.sh

--- bash/folder_handling.sh
+++ bash/folder_handling.sh
@@ -1,20 +1,20 @@
 
-# delete all files in all current subdirectorys with .h5 extension
+### delete all files in all current subdirectorys with .h5 extension
 find . -name \*.h5 -type f -delete
 
 
 
-# search all subdirectorys and rename them like their folder name + move one up
+### search all subdirectorys and rename them like their folder name + move one up
 find mirtrap -type d -not -empty -exec mv \{\}/fw.fastq.gz \{\}_fw.fastq.gz \;
 
 
 
-# delete all empty subdirectories
+### delete all empty subdirectories
 find . -type d -empty -delete
 
 
-
-
+### list largest directories from current directory
+du -sh * | sort -hr | head -n10
 
 
 
