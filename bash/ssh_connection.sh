5e0569d46cd621a1aebcb8f1dae23859af3eb703
loipf <49686556+loipf@users.noreply.github.com>
2021-10-06 11:36:26 +0200

    Update ssh_connection.sh

--- bash/ssh_connection.sh
+++ bash/ssh_connection.sh
@@ -12,3 +12,14 @@ sshfs -ocache=yes,kernel_cache,compression=no,default_permissions,idmap=user,uid
 
 
 
+### LOW SECURITY !! faster parallelized rsync for all subdir from dir
+### from: https://egafni.wordpress.com/2013/01/31/blazing-fast-file-transfer-with-rsync-and-ssh-hpn/
+### update: arcfour not supported anymore
+ls /dir | xargs -P 10 -I {} \ 
+rsync -avP --inplace -e "ssh -c arcfour128" dir/{} dest/
+
+
+
+
+
+
