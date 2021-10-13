35c187637200b01e2be7a6362f3fdc27241d8d38
loipf <49686556+loipf@users.noreply.github.com>
2021-10-13 14:47:09 +0200

    Update website_crawler.pl

--- perl/website_crawler.pl
+++ perl/website_crawler.pl
@@ -1,19 +1,16 @@
+#!/usr/bin/perl
+
+use warnings;
 
 
-$string="DB00295, DB00530, DB00586";
+$string="GOBP_LEUKOCYTE_MIGRATION, GOBP_RESPONSE_TO_INTERFERON_GAMMA";
 
 @ids = split(', ', $string);
 for my $n (@ids) {
-	my $url = `curl -s "https://www.drugbank.ca/drugs/$n"`;
-	if( $url =~ /Gene\sName:<\/dt><dd>(UGT1A1)<\/dd>/) {
-		#print $n."\t UGT1A1\n";
+	my $url = `curl -s "http://www.gsea-msigdb.org/gsea/msigdb/cards/$n"`;
+	if( $url =~ /Exact\ssource<\/th>\n\s+<td>(.+)<\/td>/ ) {
+		print $n."\t$1\n";
 	} else {
 		print $n."\n";
 	}
 }
-
-
-
-
-
-
