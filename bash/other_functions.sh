058e86473f06f9ef5b8e613b6d61fa443ea5d05e
loipf <49686556+loipf@users.noreply.github.com>
2021-12-02 10:19:59 +0100

    Update other_functions.sh

--- bash/other_functions.sh
+++ bash/other_functions.sh
@@ -1,15 +1,10 @@
 
 
-
 ### add timestamp before printing
 echo_time() {
     date +"%R $*"
 }
-
 echo_time "download .fa files"
-
-
-
 
 
 ### outputs number of rows and columns of a file (specify FS separator)
@@ -20,12 +15,13 @@ awk 'BEGIN{FS="\t"}END{print "COLUMN NO: "NF " ROWS NO: "NR}' file
 awk 'NR >= 400 && NR <= 405' file
 
 
-
 ### compare two files if identical
 cmp --silent file1 file2 && echo '### SUCCESS: identical files ###' || echo '### WARNING: different files ###'
 
 
-
+### compare if identical files in subfolder (first slow line by line, second only name comparison)
+diff -qr dir1 dir2
+diff <(ls -1a dir1) <(ls -1a dir2)
 
 
 
